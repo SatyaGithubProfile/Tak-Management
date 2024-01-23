590ec11208baeffb706fb427be56a649103b25bc
satya <teja-satya-sai.ryali.ext@siemens.com>
2024-01-23 17:34:38 +0530

    * Cors issue resolved (Node)

--- Node/server.js
+++ Node/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const cors = require('cors');
+app.use(cors()); // To solve the cors error while running the server & client is running local-machine
 require('./startup/db')();  // Connect mongo Db
 require('./startup/routes')(app);  // to initialize the routes
 
