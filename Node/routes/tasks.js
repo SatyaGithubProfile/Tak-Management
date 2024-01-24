eab4dbc175bf6ae72b7f72614c6f6d1decf486b2
satya <teja-satya-sai.ryali.ext@siemens.com>
2024-01-24 19:50:48 +0530

    * Middleware to the routes.

--- Node/routes/tasks.js
+++ Node/routes/tasks.js
@@ -7,41 +7,41 @@ const auth = require('../middleware/auth');
 
 
 router.get('/', [auth], async (req, res) => {
-    const tasks = await Task.find().sort('name')
-    res.send(tasks);
+  const tasks = await Task.find().sort('name')
+  res.send(tasks);
 })
 
 // Create Tasks
-router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    let task = new Task({ name: req.body.name, comment: req.body.comment });
-    task = await task.save();
-    res.send(task);
+router.post('/', [auth], async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  let task = new Task({ name: req.body.name, comment: req.body.comment });
+  task = await task.save();
+  res.send(task);
 });
 
 // http://localhost:3003/tasks/65ae407df24de52b2d78cd97
-router.put('/:id', async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.put('/:id', [auth], async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    let task = await Task.findByIdAndUpdate(req.params.id, {
-        name: req.body.name,
-        comment: req.body.comment
-    }, { new: true });
+  let task = await Task.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    comment: req.body.comment
+  }, { new: true });
   if (!task) return res.status(404).send('The Task with the given ID was not found.');
   res.send(task);
 
 })
 
-router.delete('/:id', async (req, res) => {
-    const task = await Task.findByIdAndDelete(req.params.id);
-  
-    if (!task) return res.status(404).send('The Task with the given ID was not found.');
-  
-    // res.send(task);  // To Know which record is deleted!
-    res.send({ message : "Successfully, Deleted..." });
-  });
+router.delete('/:id', [auth], async (req, res) => {
+  const task = await Task.findByIdAndDelete(req.params.id);
+
+  if (!task) return res.status(404).send('The Task with the given ID was not found.');
+
+  // res.send(task);  // To Know which record is deleted!
+  res.send({ message: "Successfully, Deleted..." });
+});
 
 
 module.exports = router;
