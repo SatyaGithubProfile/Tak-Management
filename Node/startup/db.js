4bc8439a1f1bf87fab818158645f121be3183a74
satya <teja-satya-sai.ryali.ext@siemens.com>
2024-01-29 20:39:53 +0530

    * Allignment CSS

--- Node/startup/db.js
+++ Node/startup/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
+const local = "mongodb://localhost/fullStack";
+const mongoCloud = 'mongodb+srv://teja151539:<loyjWqttlY2uzCs9>@cluster0.wlhjg3u.mongodb.net/?retryWrites=true&w=majority';
+
 module.exports = function () {
-    mongoose.connect('mongodb://localhost/fullStack').then(() => {
+    mongoose.connect(local).then(() => {
         console.log('Mongoose connected......')
     })
 }
